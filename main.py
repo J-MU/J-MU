45ffe4c292541b2b15192501c3ec7bef79262814
MIN UK JUNG <alsdnrdl001@gmail.com>
2024-06-10 17:56:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,16 @@ with open("README.md",'w',encoding='utf-8') as f:
     f.write(new_text_content)
 
 # 여기서부터는 백준 크롤링
-url = Request('https://www.acmicpc.net/status?user_id=alsdnrdl01&result_id=4', headers={'User-Agent': 'Mozilla/5.0'})
+url = Request('https://www.acmicpc.net/status?user_id=alsdnrdl01&result_id=4', headers={
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+    "Accept-Encoding": "none",
+    "Accept-Language": "en-US,en;q=0.8",
+    "Connection": "keep-alive",
+    "refere": "https://example.com",
+    "cookie": """your cookie value ( you can get that from your web page) """,
+})
 
 html = urlopen(url)
 soup = BeautifulSoup(html, 'html.parser')
